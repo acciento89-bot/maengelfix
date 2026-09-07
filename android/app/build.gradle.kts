ae37f3b9186c48d7fe5cc5aa5babd9fef149203b
Kamilunavo <acciento89@gmail.com>
2026-09-07 16:23:13 +0200

    release: bump Android to 1.0.4 (5)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.kamilunavo.maengelfix"
         minSdk = 26
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.0.3"
+        versionCode = 5
+        versionName = "1.0.4"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "APP_URL", "\"https://maengelfix.kamilunavo.com/app\"")
         buildConfigField("String", "ALLOWED_HOST", "\"maengelfix.kamilunavo.com\"")
